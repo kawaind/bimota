057032dce776bd0735d9e8cfb4d252af91b8d439
Lakshmishri <lakshmi.shri.v.a@gmail.com>
2024-08-02 15:38:57 +0200

    KAW-7711-image-center variant

--- blocks/image/image.js
+++ blocks/image/image.js
@@ -1,3 +1,8 @@
-export default async function decorate() {
-  // JS code
+import { variantsClassesToBEM } from '../../scripts/scripts.js';
+
+const variantClasses = ['center'];
+const blockName = 'image';
+
+export default async function decorate(block) {
+  variantsClassesToBEM(block.classList, variantClasses, blockName);
 }
