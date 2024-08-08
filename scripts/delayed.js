fa22895d9cffc14110e46c5a329eb69d7bb9016f
Lakshmishri <lakshmi.shri.v.a@gmail.com>
2024-08-08 09:41:16 +0200

    set up onetrust cookie management

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,33 @@
-// add delayed functionality here
+import { loadScript } from './aem.js';
+
+// OneTrust Cookies Consent Notice
+if (!window.location.pathname.includes('srcdoc')
+  && !['hlx.live', 'aem.page', 'aem.live'].some((url) => window.location.host.includes(url))) {
+  // when running on localhost in the block library host is empty but the path is srcdoc
+  // on localhost/hlx.page/hlx.live the consent notice is displayed every time the page opens,
+  // because the cookie is not persistent. To avoid this annoyance, disable unless on the
+  // production page.
+  loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
+    type: 'text/javascript',
+    charset: 'UTF-8',
+    'data-domain-script': '0190e9aa-e486-72b8-ada6-bda52c32aa2c',
+  });
+
+  window.OptanonWrapper = () => {
+    const currentOnetrustActiveGroups = window.OnetrustActiveGroups;
+
+    function isSameGroups(groups1, groups2) {
+      const s1 = JSON.stringify(groups1.split(',').sort());
+      const s2 = JSON.stringify(groups2.split(',').sort());
+
+      return s1 === s2;
+    }
+
+    window.OneTrust.OnConsentChanged(() => {
+      // reloading the page only when the active group has changed
+      if (!isSameGroups(currentOnetrustActiveGroups, window.OnetrustActiveGroups)) {
+        window.location.reload();
+      }
+    });
+  };
+}
