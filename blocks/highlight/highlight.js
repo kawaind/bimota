beaf8f69d6145ebee3d4a84fdf5f6f88ab731fe4
Tomasz Dziezyk <tomasz.dziezyk@netcentric.biz>
2024-09-24 14:55:21 +0200

    KAW-7943 Update scrolling behavior

--- blocks/highlight/highlight.js
+++ blocks/highlight/highlight.js
@@ -16,19 +16,81 @@ const setInitScaleForPicture = (block, picture) => {
     const initScale = initialWidth / maxWidth;
 
     picture.style.transform = `scale(${initScale})`;
+  }, 100);
+};
+
+const getActiveSlideIndex = (slidersContainer) => [...slidersContainer.querySelectorAll('.highlight-slide')].findIndex((slide) => slide.classList.contains('active'));
+
+const scrollToSlide = (slidersContainer, slideIndex) => {
+  slidersContainer.style.transform = `translateY(-${slideIndex * 100}%)`;
+  slidersContainer.querySelectorAll('.highlight-slide').forEach((slide, index) => {
+    const addOrRemove = index === slideIndex ? 'add' : 'remove';
+    slide.classList[addOrRemove]('active');
   });
+};
+
+const getOnScroll = (block) => {
+  const container = block.querySelector('.highlight-slides-container');
+  let prevScrollY = window.scrollY;
+  let pause = false;
+
+  const pauseForMoment = () => {
+    pause = true;
+
+    setTimeout(() => {
+      pause = false;
+    }, 1000);
+  };
+
+  pauseForMoment();
+
+  const onScroll = () => {
+    if (pause) {
+      block.scrollIntoView({ block: 'end', behavior: 'instant' });
+      return;
+    }
+
+    const activeSlideIndex = getActiveSlideIndex(container);
+    const slideCount = container.querySelectorAll('.highlight-slide').length;
+    const deltaY = window.scrollY - prevScrollY;
+
+    pauseForMoment();
+
+    if (deltaY > 0 && activeSlideIndex < slideCount - 1) {
+      scrollToSlide(container, activeSlideIndex + 1);
+      block.scrollIntoView({ block: 'end' });
+    } else if (deltaY < 0 && activeSlideIndex > 0) {
+      scrollToSlide(container, activeSlideIndex - 1);
+      block.scrollIntoView({ block: 'end' });
+    } else {
+      // remove the listener when there is no more slides in the direction
+      window.removeEventListener('scroll', onScroll, { passive: false });
+    }
+
+    prevScrollY = window.scrollY;
+  };
+
+  return onScroll;
+};
+
+const trap = (block) => {
+  const onScroll = getOnScroll(block);
+
+  window.addEventListener('scroll', onScroll, { passive: false });
+  block.scrollIntoView({ block: 'end' });
 };
 
 const observeWhenBlockIsFullyVisible = (block) => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      if (entry.isIntersecting && entry.intersectionRatio === 1) {
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.9) {
         block.classList.add('active');
+        trap(block);
       } else {
         block.classList.remove('active');
       }
     });
-  }, { threshold: [0, 1.0] });
+  }, { threshold: [0.1, 0.9] });
 
   observer.observe(block);
 };
@@ -36,15 +98,22 @@ const observeWhenBlockIsFullyVisible = (block) => {
 export default async function decorate(block) {
   const [pictureWrapper, ...slides] = block.querySelectorAll(':scope > div');
   const slidesWrapper = document.createElement('div');
+  const slidesContainer = document.createElement('div');
 
-  slides.forEach((slide) => {
+  slides.forEach((slide, index) => {
     slide.classList.add('highlight-slide');
     slide.querySelectorAll('h1, h2, h3, h4, h5, h6')
       .forEach((heading) => heading.classList.add('h1'));
+
+    if (index === 0) {
+      slide.classList.add('active');
+    }
   });
 
   slidesWrapper.classList.add('highlight-slides-wrapper');
-  slidesWrapper.append(...slides);
+  slidesContainer.classList.add('highlight-slides-container');
+  slidesContainer.append(...slides);
+  slidesWrapper.append(slidesContainer);
   block.append(slidesWrapper);
 
   const picture = pictureWrapper.querySelector('picture');
