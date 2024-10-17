414b23f7939d59fcaae4a5bae5d63cd6802a7701
Lakshmishri <lakshmi.shri.v.a@gmail.com>
2024-10-17 14:55:57 +0200

    fix fonts, add author

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -11,27 +11,38 @@ export default async function decorate(block) {
   [...textElements].forEach((heading) => {
     heading.classList.add('text-content');
   });
-  const quoteWrapper = document.createElement('div');
-  quoteWrapper.classList.add('text-quote-wrapper');
-  const quotedText = block.querySelector('strong');
-  quotedText.classList.add('h4');
-  const openQuotes = document.createRange().createContextualFragment(`
-    <div class="text-open-quotes">
-      <img data-icon-name="arrow" src="/icons/quote-open.svg" alt="" loading="lazy">
-    </div>
-  `);
-  const closeQuotes = document.createRange().createContextualFragment(`
-    <div class="text-close-quotes">
-      <img data-icon-name="arrow" src="/icons/quote-close.svg" alt="" loading="lazy">
-    </div>
-  `);
-  quoteWrapper.append(openQuotes, quotedText, closeQuotes);
 
   gatherButtons(block.querySelectorAll('.button-container'));
 
   const buttonContainer = block.querySelector('.button-container');
-  buttonContainer.classList.remove('text-content');
-  textContainer.insertBefore(quoteWrapper, buttonContainer);
+  buttonContainer?.classList.remove('text-content');
 
-  quoteWrapper.previousElementSibling.remove();
+  let quoteWrapper;
+  const quotedText = block.querySelector('.text-content strong');
+  if (quotedText) {
+    quoteWrapper = document.createElement('div');
+    quoteWrapper.classList.add('text-quote-wrapper');
+    quotedText.classList.add('h4');
+    const quotes = document.createElement('div');
+    quotes.classList.add('text-quotes-container');
+    const nextEle = quotedText.parentElement.nextElementSibling;
+    if (!nextEle.classList.contains('button-container')) {
+      quotes.append(nextEle);
+      nextEle.classList.remove('text-content');
+    }
+    quotes.prepend(quotedText);
+    const openQuotes = document.createRange().createContextualFragment(`
+      <div class="text-open-quotes">
+        <img data-icon-name="arrow" src="/icons/quote-open.svg" alt="" loading="lazy">
+      </div>
+    `);
+    const closeQuotes = document.createRange().createContextualFragment(`
+      <div class="text-close-quotes">
+        <img data-icon-name="arrow" src="/icons/quote-close.svg" alt="" loading="lazy">
+      </div>
+    `);
+    quoteWrapper.append(openQuotes, quotes, closeQuotes);
+    textContainer.insertBefore(quoteWrapper, buttonContainer);
+    quoteWrapper.previousElementSibling.remove();
+  }
 }
