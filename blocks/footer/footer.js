52681548e870f617cab5f6330473ca73039f5f0b
Lakshmishri <lakshmi.shri.v.a@gmail.com>
2025-03-28 14:18:10 +0100

    fix loading footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -9,7 +9,7 @@ import { addTitleAttributeToIconLink } from '../../scripts/helpers.js';
 export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : `${getRootPath}/footer`;
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : `${getRootPath()}/footer`;
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
