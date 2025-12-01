b9d42559194efb3031cd8d1b283a93c7600aee48
Sybrand Lubbe <sybrand@gacreations.co.za>
2025-12-01 15:27:56 +0100

    Adding New Languages for Dealer Maps

--- blocks/dealer-locator/dealer-locator.js
+++ blocks/dealer-locator/dealer-locator.js
@@ -1,7 +1,7 @@
 import { loadScript, getMetadata } from '../../scripts/aem.js';
 
 const getLanguage = (defaultLang = 'it') => {
-  const langs = ['en', 'it', 'ja', 'es'];
+  const langs = ['en', 'it', 'ja', 'es', 'fr', 'de', 'nl'];
   const foundLang = langs.find((lang) => window.location.pathname.includes(`/${lang}/`));
   return foundLang ?? defaultLang;
 };
