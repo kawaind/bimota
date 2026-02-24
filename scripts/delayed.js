0a9c7caded26912541b355e9e215439b99343da2
Adriel Tosi <adrielstosi@gmail.com>
2026-02-24 09:43:05 +0100

    KAW-10006 - Fix cookie imprint and privacy links

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -8,9 +8,17 @@ if (!window.location.pathname.includes('srcdoc')
   // on localhost/hlx.page/hlx.live the consent notice is displayed every time the page opens,
   // because the cookie is not persistent. To avoid this annoyance, disable unless on the
   // production page.
-  const { locale } = getLocale();
+  const { language, country, locale } = getLocale();
+  let cookiesLinks
 
-  loadScript(`https://cloud.ccm19.de/app.js?apiKey=c7d2f47f3259dd5a137414a641f559ee48d81e684564ca8f&amp;domain=67e136a8868b63fcba0a4022&amp;lang=${locale}`, {
+  await fetch("/cookies-links.json")
+    .then(response => response.json())
+    .then(response => {
+      cookiesLinks = response.data;
+    })
+  window.addEventListener('ccm19WidgetLoaded', updateCookieLinks.bind(null, country, language, cookiesLinks))
+    
+  await loadScript(`https://cloud.ccm19.de/app.js?apiKey=c7d2f47f3259dd5a137414a641f559ee48d81e684564ca8f&amp;domain=67e136a8868b63fcba0a4022&amp;lang=${locale}`, {
     type: 'text/javascript',
     charset: 'UTF-8',
   });
@@ -31,6 +39,25 @@ if (!window.location.pathname.includes('srcdoc')
       }
     });
   };
+}
+
+function updateCookieLinks(country, language, cookiesLinks) {
+  const widgetLinks = document.querySelectorAll('.ccm-widget .ccm-modal--footer a');
+  const panelControlLinks = document.querySelectorAll('.ccm-control-panel .ccm-modal--footer a');
+
+  const languagePath = `/${country}/${language}`;
+  const newPaths = cookiesLinks.find(item => item.path === languagePath);
+
+  if (cookiesLinks && newPaths) {
+    if (widgetLinks.length > 1) {
+      widgetLinks[0].href = newPaths.cookieUrl;
+      widgetLinks[1].href = newPaths.privacyUrl;
+    }
+    if (panelControlLinks.length > 1) {
+      panelControlLinks[0].href = newPaths.cookieUrl;
+      panelControlLinks[1].href = newPaths.privacyUrl;
+    }
+  }
 }
 
 function injectScript(src, crossOrigin = '') {
