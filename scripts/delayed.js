d8dc32880e728b7adfaa70339f6df2bd0ed0b3a1
Sybrand Lubbe <sybrand@gacreations.co.za>
2026-05-13 17:16:03 +0200

    revert fixes

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,17 +4,21 @@ import { getLocale } from './helpers.js';
 // OneTrust Cookies Consent Notice
 if (!window.location.pathname.includes('srcdoc')
   && !['localhost'].some((url) => window.location.host.includes(url))) {
+  // when running on localhost in the block library host is empty but the path is srcdoc
+  // on localhost/hlx.page/hlx.live the consent notice is displayed every time the page opens,
+  // because the cookie is not persistent. To avoid this annoyance, disable unless on the
+  // production page.
   const { language, country, locale } = getLocale();
-  let cookiesLinks;
+  let cookiesLinks
 
-  await fetch('/cookies-links.json')
+  await fetch("/cookies-links.json")
     .then(response => response.json())
     .then(response => {
       cookiesLinks = response.data;
-    });
-  window.addEventListener('ccm19WidgetLoaded', updateCookieLinks.bind(null, country, language, cookiesLinks));
-
-  await loadScript('https://cloud.ccm19.de/app.js?apiKey=c7d2f47f3259dd5a137414a641f559ee48d81e684564ca8f&amp;domain=67e136a8868b63fcba0a4022&amp;lang=${locale}', {
+    })
+  window.addEventListener('ccm19WidgetLoaded', updateCookieLinks.bind(null, country, language, cookiesLinks))
+    
+  await loadScript(`https://cloud.ccm19.de/app.js?apiKey=c7d2f47f3259dd5a137414a641f559ee48d81e684564ca8f&amp;domain=67e136a8868b63fcba0a4022&amp;lang=${locale}`, {
     type: 'text/javascript',
     charset: 'UTF-8',
   });
@@ -41,7 +45,7 @@ function updateCookieLinks(country, language, cookiesLinks) {
   const widgetLinks = document.querySelectorAll('.ccm-widget .ccm-modal--footer a');
   const panelControlLinks = document.querySelectorAll('.ccm-control-panel .ccm-modal--footer a');
 
-  const languagePath = '/${country}/${language}';
+  const languagePath = `/${country}/${language}`;
   const newPaths = cookiesLinks.find(item => item.path === languagePath);
 
   if (cookiesLinks && newPaths) {
